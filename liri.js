37c1eb91d9bc71e741f2ec4e2061f60393c45fdb
Jeff Romine <jeffromine@overstock.com>
2017-08-20 15:49:44 -0600

    Finish most of the assignment except for output formatting and do-what-it-says.

--- liri.js
+++ liri.js
@@ -1,35 +1,68 @@
-var fs = require('fs');
 var urlencode = require('urlencode');
 var twitterKeys = require('./keys').twitterKeys;
+var spotifyKeys = require('./spotify-keys').spotifyKeys;
 var Twitter = require('twitter');
 var extend = require('util')._extend;
 var request = require('request');
+var Spotify = require('node-spotify-api');
+var omdbKey = '40e9cece';
 
 var command = process.argv[2];
+var args    = process.argv.slice(3);
 
 if (command) {
   switch (command) {
   case 'my-tweets':
     myTweets();
     break;
+  case 'spotify-this-song':
+    spotifyThisSong.apply(this, args);
+    break;
+  case 'movie-this':
+    spotifyThisSong.apply(this, args);
+    break;
   }
 }
 
 function myTweets() {
-  requestBearerToken((accessToken) => {
-    getTweets(accessToken,
-              (err, tweets, response) => {
-                if (err) {
-                  console.log(JSON.stringify(error,null,2));
-                }
-                else {
-                  console.log(JSON.stringify(tweets,null,2));
-                };
-              });
-  });
+  requestBearerToken(
+    (accessToken) => {
+      getTweets(accessToken,
+                (err, tweets, response) => {
+                  if (err) {
+                    console.log(JSON.stringify(error,null,2));
+                  }
+                  else {
+                    console.log(JSON.stringify(tweets,null,2));
+                  };
+                });
+    });
+}
+
+function spotifyThisSong(title) {
+  var spotify = new Spotify(
+    spotifyKeys);
+
+  if (!title) {
+    console.log("No title specified");
+    return null;
+  }
+  return spotify
+    .search({ type: 'track', query: title })
+    .then(function(response) {
+      console.log(JSON.stringify(response,null,2));})
+    .catch(function(err) {
+      console.log(JSON.stringify(err,null,2));});
+}
+
+function getTweets(accessToken, callback) {
+  var twitterClient = new Twitter(extend(twitterKeys,{bearer_token: accessToken}));
+  var params = {screen_name: 'jrominetest'};
+  twitterClient.get('statuses/user_timeline', params, callback);
 }
 
 function requestBearerToken(callback) {
+  var authTokenUrl = 'https://api.twitter.com/oauth2/token';
   var credentials    =
         urlencode(twitterKeys.consumer_key) +
         ':' +
@@ -37,7 +70,7 @@ function requestBearerToken(callback) {
   var encodedCredentials = base64Encode(credentials);
 
   request({
-    url: 'https://api.twitter.com/oauth2/token',
+    url: authTokenUrl,
     method: 'POST',
     headers: {
       "Authorization":"Basic " + encodedCredentials,
@@ -58,8 +91,23 @@ function base64Encode(s) {
   return new Buffer(s).toString('base64');
 }
 
-function getTweets(accessToken, callback) {
-  var twitterClient = new Twitter(extend(twitterKeys,{bearer_token: accessToken}));
-  var params = {screen_name: 'jrominetest'};
-  twitterClient.get('statuses/user_timeline', params, callback);
+
+function movieThis(movieTitle) {
+  var movieUrl =  "http://www.omdbapi.com/?apikey="
+        + omdbKey
+        + "&t="
+        + urlencode(movieTitle);
+
+  console.log("movieUrl: " + movieUrl);
+
+  request({
+    url: movieUrl,
+    method: 'GET'
+  }, function (err,res,body) {
+    if (err) {
+      console.log("err: " + JSON.stringify(err, null, 2));
+      return;
+    }
+    console.log("body: " + JSON.stringify(JSON.parse(body), null, 2));
+  });
 }
---
+++ spotify-keys.js
@@ -0,0 +1,4 @@
+exports.twitterKeys = {
+  id: '1c6da4af46d44597975f70faf18c4aa5',
+  secret: '182bc30509404f5381b7de284c432cd0'
+};
