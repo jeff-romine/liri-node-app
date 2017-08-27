07fba34e79f0a857da4716ff1ab36dcb5de6bab9
Jeff Romine <jeffromine@overstock.com>
2017-08-27 14:20:06 -0600

    Improvements

--- liri.js
+++ liri.js
@@ -8,7 +8,7 @@ const Spotify = require('node-spotify-api');
 const omdbKey = '40e9cece';
 const fs = require('fs');
 const csvParse = require('csv-parse');
-const moustache = require('mustache');
+const Handlebars = require('handlebars');
 const Entities = require('html-entities').XmlEntities;
 const entities = new Entities();
 
@@ -37,22 +37,28 @@ function printTweets(tweets) {
     tweets.forEach((tweet) => printFromTemplate(tweetTemplate, tweet));
 }
 
-const sep = "----------------------------------------";
-function printSep() {
-    console.log(sep);
-}
 function printFromTemplate(template, context) {
-    var text = entities.decode(moustache.render(template, context));
+    var text = entities.decode(template(context));
     console.log(text);
     printSep();
 }
 
-var tweetTemplateLines = [
+const sep = "----------------------------------------";
+function printSep() {
+    console.log(sep);
+}
+
+const tweetTemplateLines = [
     "created: {{created_at}}",
     "text:    {{text}}"
 ];
 
-var tweetTemplate = tweetTemplateLines.join('\n');
+const tweetTemplate = compileTemplateLines(tweetTemplateLines);
+
+
+function compileTemplateLines(templateLines) {
+    return Handlebars.compile(templateLines.join("\n"));
+}
 
 function myTweets() {
     requestBearerToken(
@@ -64,18 +70,19 @@ function myTweets() {
                     }
                     else {
                         printTweets(tweets);
-                    };
+                    }
+                    ;
                 });
         });
 }
 
-var songTemplateLines = [
-  "name:         {{name}}",
-  "artists:      {{artists}}",
-  "preview link: {{previewUrl}}"
+const songTemplateLines = [
+    "name:         {{name}}",
+    "artists:      {{artists}}",
+    "preview link: {{previewUrl}}"
 ];
 
-var songTemplate = songTemplateLines.join("\n");
+const songTemplate = compileTemplateLines(songTemplateLines);
 
 function spotifyThisSong(title) {
 
@@ -94,13 +101,13 @@ function spotifyThisSong(title) {
 
             var item = response.tracks.items[0];
 
-          var song = {
+            var song = {
                 name: item.name,
                 artists: item.artists.map((artist) => artist.name).join(","),
                 previewUrl: item.preview_url
             };
-          console.log(sep);
-          printFromTemplate(songTemplate,song);
+            console.log(sep);
+            printFromTemplate(songTemplate, song);
         })
         .catch(function (err) {
             console.log(JSON.stringify(err, null, 2));
@@ -143,18 +150,18 @@ function base64Encode(s) {
     return new Buffer(s).toString('base64');
 }
 
-var movieTemplateLines = [
-  "Title:                  {{Title}}",
-  "Year:                   {{Year}}",
-  "IMDB Rating:            {{imdbRating}}",
-  "Rotten Tomatoes Rating: {{rottenTomatoesRating}}",
-  "Country:                {{Country}}",
-  "Language:               {{Language}}",
-  "Plot:                   {{Plot}}",
-  "Actors:                 {{Actors}}"
+const movieTemplateLines = [
+    "Title:                  {{Title}}",
+    "Year:                   {{Year}}",
+    "IMDB Rating:            {{imdbRating}}",
+    "Rotten Tomatoes Rating: {{rottenTomatoesRating}}",
+    "Country:                {{Country}}",
+    "Language:               {{Language}}",
+    "Plot:                   {{Plot}}",
+    "Actors:                 {{Actors}}"
 ];
 
-var movieTemplate = movieTemplateLines.join("\n");
+const movieTemplate = compileTemplateLines(movieTemplateLines);
 
 function movieThis(movieTitle) {
 
@@ -168,21 +175,21 @@ function movieThis(movieTitle) {
         url: movieUrl,
         method: 'GET'
     }, function (err, res, body) {
-      if (err) {
+        if (err) {
             console.log("err: " + JSON.stringify(err, null, 2));
-        return;
-      }
-      var movie = JSON.parse(body);
-      movie.rottenTomatoesRating = movie.Ratings.find((rating) => (rating.Source === 'Rotten Tomatoes')).Value;
-      printSep();
-      printFromTemplate(movieTemplate,movie);
+            return;
+        }
+        var movie = JSON.parse(body);
+        movie.rottenTomatoesRating = movie.Ratings.find((rating) => (rating.Source === 'Rotten Tomatoes')).Value;
+        printSep();
+        printFromTemplate(movieTemplate, movie);
     });
 }
 
 function doWhatItSays(commandFileParam) {
     var commandFilePath = commandFileParam || './random.txt';
     var commandFileContents = fs.readFileSync(commandFilePath);
-    csvParse(commandFileContents, (err, commandLines) => {
+    csvParse(commandFileContents, {relax_column_count: true}, (err, commandLines) => {
         commandLines.forEach(
             (commandLine) => {
                 processCommand(commandLine[0], commandLine.slice(1));
